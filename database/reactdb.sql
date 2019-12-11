1c3b1c47ae7ef409bafe92a9061628c8f33e06ab
EchoPrasetyo <me.serj.adam@gmail.com>
2019-12-11 14:12:52 +0700

    update database sql password hash

--- database/reactdb.sql
+++ database/reactdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Des 2019 pada 05.23
+-- Waktu pembuatan: 11 Des 2019 pada 08.11
 -- Versi server: 10.1.37-MariaDB
 -- Versi PHP: 7.3.1
 
@@ -50,7 +50,8 @@ INSERT INTO `feed` (`feed_id`, `feed`, `user_id`) VALUES
 (296, 'dataFeed 7', 1),
 (297, 'dataFeed 8', 1),
 (300, 'dataFeed 9', 1),
-(301, 'dataFeed 10', 1);
+(301, 'dataFeed 10', 1),
+(302, 'cek 022', 1);
 
 -- --------------------------------------------------------
 
@@ -80,8 +81,7 @@ INSERT INTO `karyawan` (`id_karyawan`, `KTP`, `nama`, `no_hp`) VALUES
 (171, '009867111115', 'nara 1', '0897-6789-1111'),
 (172, '009867111116', 'nara 2', '0897-6789-1111'),
 (173, '009867111117', 'nara 3', '0897-6789-1111'),
-(175, '009867111117', 'rara', '0897-6789-1111'),
-(177, '009867111117', 'rara 1', '0897-6789-1111');
+(175, '009867111117', 'rara', '0897-6789-1111');
 
 -- --------------------------------------------------------
 
@@ -102,9 +102,13 @@ CREATE TABLE `karyawannew` (
 --
 
 INSERT INTO `karyawannew` (`id_karyawan`, `nama`, `KTP`, `no_hp`, `id_kota`) VALUES
-(1, 'rara', '009867111117', '0897-6789-1111', 3),
-(2, 'rara 01', '009867111116', '0897-6789-1111', 5),
-(4, 'rara 02', '009867111115', '0897-6789-1111', 3);
+(1, 'rara', '009867111117', '0897-6789-1111', 1),
+(2, 'rara 01', '009867111116', '0897-6789-1111', 6),
+(4, 'rara 02', '009867111115', '0897-6789-1111', 1),
+(5, 'rara 03', '009867111115', '0897-6789-1111', 3),
+(6, 'rara 04', '009867111115', '0897-6789-1111', 1),
+(16, 'Rubi', '009867111115', '0897-6789-1111', 18),
+(17, 'rara 01', '009867111117', '0897-6789-1111', 18);
 
 -- --------------------------------------------------------
 
@@ -127,7 +131,22 @@ INSERT INTO `select_kota` (`id_kota`, `kota`) VALUES
 (3, 'Jakarta Selatan'),
 (4, 'Jakarta Utara'),
 (5, 'Jakarta Timur'),
-(6, 'Bekasi');
+(6, 'Bekasi'),
+(7, 'Semarang'),
+(8, 'Banten'),
+(9, 'Cilacap'),
+(10, 'Bali'),
+(11, 'Palembang'),
+(12, 'Pati'),
+(13, 'Karawang'),
+(14, 'Singapura'),
+(15, 'Ambon'),
+(16, 'Papua'),
+(17, 'Cikarang'),
+(18, 'Purwokerto'),
+(19, 'Maluku'),
+(20, 'Aceh'),
+(21, 'Banyumas');
 
 -- --------------------------------------------------------
 
@@ -148,10 +167,34 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `name`, `email`) VALUES
-(1, 'eko', 'eko', 'eko', 'e@y.com'),
-(2, 'eko', 'eko', 'eko', 'e@y.com'),
-(3, 'noname', 'noname', 'noname', 'mohamad@yahoo.com'),
-(5, 'rara', 'rara', 'rara', 'rr@ymail.com');
+(6, 'magang', '49364cdd62e72c03a31ad7f026f9d24c7bcc6e96', 'ani', 'ani@yahoo.com'),
+(7, 'mohamad', '2cd61bb25184fbc299355ec6e5ef232592d082cd', 'mohamad eko', 'eko@ymail.com'),
+(8, 'eko', 'f8ad2eb1d1ffa3324bf3b6ce1979086459f400f3', 'eko prasetyo', 'me.serj.adam@gmail.com'),
+(9, 'serj', '5496f58b0e6fe96b4f0c8e28c24cc8a3afbfc824', 'serj adam', 'echorockers3@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in struktur untuk tampilan `view_karyawan`
+-- (Lihat di bawah untuk tampilan aktual)
+--
+CREATE TABLE `view_karyawan` (
+`id_karyawan` int(100)
+,`nama` varchar(255)
+,`KTP` varchar(255)
+,`no_hp` varchar(255)
+,`id_kota` int(100)
+,`kota` varchar(255)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur untuk view `view_karyawan`
+--
+DROP TABLE IF EXISTS `view_karyawan`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_karyawan`  AS  select `karyawannew`.`id_karyawan` AS `id_karyawan`,`karyawannew`.`nama` AS `nama`,`karyawannew`.`KTP` AS `KTP`,`karyawannew`.`no_hp` AS `no_hp`,`karyawannew`.`id_kota` AS `id_kota`,`select_kota`.`kota` AS `kota` from (`select_kota` join `karyawannew` on((`karyawannew`.`id_kota` = `select_kota`.`id_kota`))) ;
 
 --
 -- Indexes for dumped tables
@@ -195,7 +238,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `feed`
 --
 ALTER TABLE `feed`
-  MODIFY `feed_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=302;
+  MODIFY `feed_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=304;
 
 --
 -- AUTO_INCREMENT untuk tabel `karyawan`
@@ -207,19 +250,19 @@ ALTER TABLE `karyawan`
 -- AUTO_INCREMENT untuk tabel `karyawannew`
 --
 ALTER TABLE `karyawannew`
-  MODIFY `id_karyawan` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_karyawan` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT untuk tabel `select_kota`
 --
 ALTER TABLE `select_kota`
-  MODIFY `id_kota` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_kota` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
